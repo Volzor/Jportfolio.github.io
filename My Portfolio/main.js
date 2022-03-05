7562bdc7ee7d43ac8617a8f3522a124b701640c0
Volzor <96282373+Volzor@users.noreply.github.com>
2022-03-05 00:17:47 -0800

    Add files via upload

---
+++ My Portfolio/main.js
@@ -0,0 +1,9 @@
+// event handler function to increase size of image on mousedown
+function imageClick() {
+    Ellen = document.getElementById("selfie");
+    Ellen.style.width = "500px";
+    Ellen.style.height = "500px";
+}
+
+// event handler
+    Document.onmousedown("imageClick");
