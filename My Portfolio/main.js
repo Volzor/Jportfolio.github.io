551a7e7a2e0af0acd97757c60734981764017c78
Volzor <96282373+Volzor@users.noreply.github.com>
2022-03-05 01:29:49 -0800

    Added JS features
    Added event handlers to zoom in on portfolio pic and added current date to be displayed.

--- My Portfolio/main.js
+++ My Portfolio/main.js
@@ -1,9 +1,27 @@
-// event handler function to increase size of image on mousedown
+// variable to call our portfolio pic
+let ellen = document.getElementById("selfie");
+
+// event handler to increase portfolio picture on mousedown
 function imageClick() {
-    Ellen = document.getElementById("selfie");
-    Ellen.style.width = "500px";
-    Ellen.style.height = "500px";
+    ellen.style.width = "500px";
+    ellen.style.height = "500px";
 }
 
-// event handler
-    Document.onmousedown("imageClick");
+// event handler to reset pic size 
+function reset() {
+    ellen.style = '';
+}
+ 
+// event handler for imageClick
+ellen.addEventListener("mousedown", imageClick);
+ellen.addEventListener("mouseup", reset);
+
+
+// date 
+let dates = document.getElementById("date");
+
+function setDate() {
+    dates.innerHTML = "The current date is: " + new Date;
+}
+
+setDate();
